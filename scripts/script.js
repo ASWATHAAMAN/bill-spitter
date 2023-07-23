0b1cbe8bf8de9bd3840e3f2d5a655ec7e5e97b36
ASWATHAAMAN A C <aswathaaman19@gmail.com>
2023-07-23 21:45:55 +0530

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
 `use strict`;
+// select elements
+const paraHeadingEl = document.getElementById(`para-heading`);
+const rupeesEl = document.getElementById(`rupees`);
+const peopleEl = document.getElementById(`people`);
+const calculateEl = document.getElementById(`calculate`);
+const resetEl = document.getElementById(`reset`);
+
+// global scope
+
+// functions
+
+// addEvent Listeners
+
+// initial settings
